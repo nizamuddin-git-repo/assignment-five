f025a532a6889fe80fa7c1c6cbf7f65b66300617
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-14 15:38:23 +0600

    add button count

--- script.js
+++ script.js
@@ -4,9 +4,13 @@ const allbtn = document.getElementsByClassName('add-btn');
 
 let count = 0;
 
+let count2 = 40;
+
 for(btn of allbtn){
     btn.addEventListener('click', function (e) {
        count = count + 1;
+
+       count2 = count2 -1;
 
 
         const placeName = e.target.parentNode.childNodes[3].innerText;
@@ -15,7 +19,7 @@ for(btn of allbtn){
 
         const price = document.getElementById('price').innerText;
 
-        // const busClass = document.getElementById('bus-class').innerText;
+        const busClass = document.getElementById('Available').innerText;
 
         // const seatPrice = document.getElementById('seat-price').innerText;
 
@@ -26,36 +30,58 @@ for(btn of allbtn){
         const p = document.createElement('p')
         p.innerText = placeName;
         const p2 = document.createElement('p2')
-        p2.innerText = price;
+        p2.innerText = busClass;
+        const p3 = document.createElement('p3')
+        p3.innerText = price;
 
-        // const p3 = document.createElement('p3')
-        // p.innerText = convertPrice;
+       
+        const seatLeft = document.getElementById('seat-left')
+
+        const convertSeatLeft = parseInt(seatLeft)
+
+        document.getElementById('seat-left').innerText = convertSeatLeft;
+
+        
+
+
 
         SelectedDiv.appendChild(p);
         SelectedDiv.appendChild(p2);
+        SelectedDiv.appendChild(p3);
 
 
-        const totalPrice = document.getElementById('total-price').innerText;
+        totalPrice('total-price', parseInt(price))
 
-        const convertTotalPrice = parseInt(totalPrice);
+        grandTotal('grand-total', parseInt(price) )
+    
 
-        const total = convertTotalPrice + parseInt(price);
 
-
-        const grandTotal = document.getElementById('grand-total').innerText;
-
-        const convertGrandTotal = parseInt(grandTotal);
-
-        const totalGrand = convertGrandTotal + parseInt(price);
-
-        setInnerText('grand-total', totalGrand);
-        setInnerText('total-price', total)
        setInnerText('seat-count', count);
+
+       
+       setInnerText('seat-left', count2);
     });
 }
-
 
 
 function setInnerText(id, value){
     document.getElementById(id).innerText = value;
 }
+
+
+function totalPrice(id, value) {
+    const totalPrice = document.getElementById(id).innerText;
+    const convertTotalPrice = parseInt(totalPrice);
+    const total = convertTotalPrice + parseInt(value);
+    setInnerText('total-price', total);
+
+}
+
+
+function grandTotal(id, value) {
+    const totalPrice = document.getElementById(id).innerText;
+    const convertTotalPrice = parseInt(totalPrice);
+    const totalGrand = convertTotalPrice + parseInt(value);
+    setInnerText('grand-total', totalGrand);
+}
+
