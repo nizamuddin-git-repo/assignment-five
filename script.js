5e03ec42eddd579a789e0b0a1844c61a733c73a7
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-15 14:31:15 +0600

    add tha function call

--- script.js
+++ script.js
@@ -9,40 +9,26 @@ let count2 = 40;
 for(btn of allbtn){
     btn.addEventListener('click', function (e) {
        count = count + 1;
-
        count2 = count2 -1;
 
 
         const placeName = e.target.parentNode.childNodes[3].innerText;
 
-        const SelectedDiv =document.getElementById('Selected-div');
 
+        const SelectedDiv = document.getElementById('Selected-div');
         const price = document.getElementById('price').innerText;
-
         const busClass = document.getElementById('Available').innerText;
 
-        // const seatPrice = document.getElementById('seat-price').innerText;
-
-        // const convertPrice = parseInt(seatPrice);
-        
+        e.target.style.backgroundColor = "green";
 
 
         const p = document.createElement('p')
-        p.innerText = placeName;
         const p2 = document.createElement('p2')
-        p2.innerText = busClass;
         const p3 = document.createElement('p3')
+
+        p.innerText = placeName;
+        p2.innerText = busClass;
         p3.innerText = price;
-
-       
-        const seatLeft = document.getElementById('seat-left')
-
-        const convertSeatLeft = parseInt(seatLeft)
-
-        document.getElementById('seat-left').innerText = convertSeatLeft;
-
-        
-
 
 
         SelectedDiv.appendChild(p);
@@ -51,15 +37,12 @@ for(btn of allbtn){
 
 
         totalPrice('total-price', parseInt(price))
-
         grandTotal('grand-total', parseInt(price) )
     
 
-
        setInnerText('seat-count', count);
-
-       
        setInnerText('seat-left', count2);
+
     });
 }
 
