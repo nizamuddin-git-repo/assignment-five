e6f7cdbc855c98bf6f87073a065a6612dc120c6c
nizamuddin0172 <neom01723441081@gmail.com>
2024-03-18 15:24:36 +0600

    add valid total price

--- script.js
+++ script.js
@@ -1,70 +1,81 @@
 
 
-const allbtn = document.getElementsByClassName('add-btn');
-
-let count = 0;
-
-let count2 = 40;
-
-for(btn of allbtn){
-    btn.addEventListener('click', function (e) {
-       count = count + 1;
-       count2 = count2 -1;
 
 
-        const placeName = e.target.parentNode.childNodes[3].innerText;
+const allbtn = document.getElementsByClassName("add-btn");
 
 
-        const SelectedDiv = document.getElementById('Selected-div');
-        const price = document.getElementById('price').innerText;
-        const busClass = document.getElementById('Available').innerText;
+for (btn of allbtn) {
+  btn.addEventListener("click", function (e) {
+   
 
-        e.target.style.backgroundColor = "green";
+    const placeName = e.target.parentNode.childNodes[3].innerText;
+
+    const price = document.getElementById("price").innerText;
+    const busClass = document.getElementById("Available").innerText;
+    const SelectedDiv = document.getElementById("Selected-div");
+
+    e.target.style.backgroundColor = "green";
+
+    e.target.setAttribute('disabled', false);
+
+    const setSeatCount = getConvertValue('seat-count');
+    if(setSeatCount + 1 > 4){
+        alert('not limit ')
+        return;
+    }
+
+    const seatleft = getConvertValue('seat-left')
+    document.getElementById('seat-left').innerText = seatleft - 1;
+
+    const seatCount = getConvertValue('seat-count');
+    document.getElementById('seat-count').innerText = seatCount + 1;
+
+    const p = document.createElement("p");
+    const p2 = document.createElement("p2");
+    const p3 = document.createElement("p3");
+
+    p.innerText = placeName;
+    p2.innerText = busClass;
+    p3.innerText = price;
+
+    SelectedDiv.appendChild(p);
+    SelectedDiv.appendChild(p2);
+    SelectedDiv.appendChild(p3);
+
+    updateTotalPrice(price);
+    updateGrandTotal();
+  });
+}
 
 
-        const p = document.createElement('p')
-        const p2 = document.createElement('p2')
-        const p3 = document.createElement('p3')
-
-        p.innerText = placeName;
-        p2.innerText = busClass;
-        p3.innerText = price;
-
-
-        SelectedDiv.appendChild(p);
-        SelectedDiv.appendChild(p2);
-        SelectedDiv.appendChild(p3);
-
-
-        totalPrice('total-price', parseInt(price))
-        grandTotal('grand-total', parseInt(price) )
+function updateGrandTotal(Coupon) {
     
+  const totalPrice = getConvertValue("total-price");
 
-       setInnerText('seat-count', count);
-       setInnerText('seat-left', count2);
+  if (Coupon == undefined) {
+    document.getElementById("grand-total").innerText = totalPrice;
+  } else {
+    const couponCode = document.getElementById("apply-Coupon").value;
 
-    });
+    if (couponCode == "nizam") {
+      const discount = totalPrice * 0.2;
+      document.getElementById("grand-total").innerText = totalPrice - discount;
+    } else {
+      alert("please enter valid coupon code");
+    }
+  }
 }
 
-
-function setInnerText(id, value){
-    document.getElementById(id).innerText = value;
+function updateTotalPrice(price) {
+  const totalPrice = getConvertValue("total-price");
+  const sum = totalPrice + parseInt(price);
+  document.getElementById("total-price").innerText = sum;
 }
 
-
-function totalPrice(id, value) {
-    const totalPrice = document.getElementById(id).innerText;
-    const convertTotalPrice = parseInt(totalPrice);
-    const total = convertTotalPrice + parseInt(value);
-    setInnerText('total-price', total);
-
-}
-
-
-function grandTotal(id, value) {
-    const totalPrice = document.getElementById(id).innerText;
-    const convertTotalPrice = parseInt(totalPrice);
-    const totalGrand = convertTotalPrice + parseInt(value);
-    setInnerText('grand-total', totalGrand);
+function getConvertValue(id) {
+  const Price = document.getElementById(id).innerText;
+  const convertPrice = parseInt(Price);
+  return convertPrice;
 }
 
